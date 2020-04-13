8ede9f2ff4613d990a9d43c2bed4fb2c6f6b446b
devansh-dvj <devansh.dvj@gmail.com>
2020-04-13 12:05:11 +0530

    Generated components and services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
+import { ChatformComponent } from './chatform/chatform.component';
+import { FeedComponent } from './feed/feed.component';
+import { MessageComponent } from './message/message.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupFormComponent,
+    LoginFormComponent,
+    ChatroomComponent,
+    ChatformComponent,
+    FeedComponent,
+    MessageComponent,
+    UserListComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
