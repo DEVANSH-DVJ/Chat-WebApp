c71aa8f6f2cfc7b81f42264ac1a7792dc41de1a1
devansh-dvj <devansh.dvj@gmail.com>
2020-04-13 23:40:16 +0530

    Signup possible now.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+// import * as firebase from 'firebase/app';
+// import { Observable } from 'rxjs/Observable';
+// import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  newUser: any;
+  value: string;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase,
+    private router: Router) {
+      this.value = "1245";
+  }
+
+  createUser(user) {
+    this.afAuth.createUserWithEmailAndPassword(user.email, user.password)
+      .then(userCredential => {
+        this.newUser = user;
+        userCredential.user.updateProfile({
+          displayName: user.displayName
+        });
+        const status = 'online';
+        console.log(userCredential.user.uid);
+        this.setUserData(user.email, user.displayName, status, userCredential.user.uid);
+        this.router.navigate(['chat'])
+      })
+      .catch(error => console.log(error));
+  }
+
+  setUserData(email: string, displayName: string, status: string, uid: string): void {
+      const path = `users/${uid}`;
+      const data = {
+        email: email,
+        displayName: displayName,
+        status: status
+      };
+
+      this.db.object(path).update(data)
+        .catch(error => console.log(error));
+    }
+
+
 }
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
-export class SignupFormComponent implements OnInit {
+export class SignupFormComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  signUp(frm) {
+    this.authService.createUser(frm.value);
   }
-
 }
